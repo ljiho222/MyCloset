75e539ea34946c2acd0db502a938480099d2cdbd
leejiho <gd1115@naver.com>
2020-10-10 23:24:31 +0900

    commit test2

---
+++ app/src/main/java/com/example/my_closet/Add.java
@@ -0,0 +1,13 @@
+package com.example.my_closet;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Add extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
--- app/src/main/java/com/example/my_closet/MainActivity.java
+++ app/src/main/java/com/example/my_closet/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.my_closet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_add;
+    private ImageButton btn_closet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_closet = findViewById(R.id.btn_closet);
+        btn_closet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Closet.class);
+                startActivity(intent); // 옷장 추가 인텐트
+            }
+        });
+
+        btn_add = findViewById(R.id.btn_add);
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Add.class);
+                startActivity(intent); // 옷장 추가 인텐트
+            }
+        });
+
     }
 }
