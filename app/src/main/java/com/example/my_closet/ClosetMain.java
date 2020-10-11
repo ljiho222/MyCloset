ab0c99bf218b7112df18112319b2d04267a20225
leejiho <gd1115@naver.com>
2020-10-12 00:40:21 +0900

    tab 생성하기

---
+++ app/src/main/java/com/example/my_closet/ClosetMain.java
@@ -0,0 +1,40 @@
+package com.example.my_closet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ClosetMain extends AppCompatActivity {
+
+    private Button btn_add;
+    private ImageButton btn_closet;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_closetmain);
+
+        btn_closet = findViewById(R.id.btn_closet);
+        btn_closet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ClosetMain.this, Closet.class);
+                startActivity(intent); // 옷장 추가 인텐트
+            }
+        });
+
+        btn_add = findViewById(R.id.btn_add);
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ClosetMain.this, Add.class);
+                startActivity(intent); // 옷장 추가 인텐트
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/my_closet/MainActivity.java
+++ app/src/main/java/com/example/my_closet/MainActivity.java
@@ -1,40 +1,24 @@
 package com.example.my_closet;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.TabHost;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Button btn_add;
-    private ImageButton btn_closet;
-
+public class MainActivity extends TabActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_closet = findViewById(R.id.btn_closet);
-        btn_closet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Closet.class);
-                startActivity(intent); // 옷장 추가 인텐트
-            }
-        });
+        TabHost tabHost = getTabHost();
 
-        btn_add = findViewById(R.id.btn_add);
-        btn_add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Add.class);
-                startActivity(intent); // 옷장 추가 인텐트
-            }
-        });
+        //첫번째 탭
+       tabHost.addTab(tabHost.newTabSpec("Closet").setIndicator("옷장").setContent(new Intent(this,ClosetMain.class)));
+
+        //두번째 탭
+        tabHost.addTab(tabHost.newTabSpec("Style").setIndicator("코디").setContent(new Intent(this,Style.class)));
+
 
     }
 }
